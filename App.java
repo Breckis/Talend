c845d315f72f6065b8cde4ce3f88b2b47e768874
jjpotgieter <jpotgieter@breckis.com>
2015-11-18 14:46:58 -0500

    testing Ericks git

--- App.java
+++ App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( DateRoutines.convertDate(DateRoutines.addDatePart("03/08/1990","MM/dd/yyyy", "yyyy", "31" ),"MM/dd/yyyy"));
+        System.out.println( DateRoutines.convertDate(DateRoutines.addDatePart("03/08/1990","MM/dd/yyyy", "yyyy", "4" ),"MM/dd/yyyy"));
     }
 }
